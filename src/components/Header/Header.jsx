0f8aff695a25a59c0fb331e21f71d79c623214cb
Mxctr1 <social+github@elmoctar.com>
2023-05-07 07:14:59 -0400

    Done everything

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,54 @@
 import React from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+import { useState } from "react";
+import { Link } from "react-scroll";
 
 const Header = () => {
+	const mobile = window.innerWidth <= 768 ? true : false;
+	const [menuOpened, setMenuOpened] = useState(false);
 	return (
 		<div className="header">
 			<img className="logo" src={Logo} alt="" />
-
-			<ul className="header-menu">
-				<li>Home</li>
-				<li>Programs</li>
-				<li>Why us </li>
-				<li>Plans</li>
-				<li>Testimonials</li>
-			</ul>
+			{menuOpened === false && mobile === true ? (
+				<div
+					style={{
+						backgroundColor: "var(--appColor)",
+						padding: "0.5rem",
+						borderRadius: "5px",
+					}}
+					onClick={() => setMenuOpened(true)}
+				>
+					{" "}
+					<img src={Bars} alt="" style={{ width: "1.5", height: "1.5rem" }} />
+				</div>
+			) : (
+				<ul className="header-menu">
+					<li>
+						<Link onClick={() => setMenuOpened(false)}>Home</Link>
+					</li>
+					<li>
+						<Link onClick={() => setMenuOpened(false)}>Programs</Link>
+					</li>
+					<li>
+						<Link onClick={() => setMenuOpened(false)}>Why us</Link>
+					</li>
+					<li>
+						<Link onClick={() => setMenuOpened(false)}>Plans</Link>
+					</li>
+					<li>
+						<Link
+							onClick={() => setMenuOpened(false)}
+							to="Testimonials"
+							spy={true}
+							smooth={true}
+						>
+							Testimonials
+						</Link>
+					</li>
+				</ul>
+			)}
 		</div>
 	);
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,11 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
 
 const Hero = () => {
 	const transition = { type: "spring", duration: 3 };
+	const mobile = window.innerWidth <= 768 ? true : false;
 
 	return (
 		<div className="hero">
@@ -20,7 +22,7 @@ const Hero = () => {
 
 				<div className="the-best-ad">
 					<motion.div
-						initial={{ left: "238px" }}
+						initial={{ left: mobile ? "178px" : "238px" }}
 						whileInView={{ left: "9px" }}
 						transition={{ ...transition, type: "tween" }}
 					></motion.div>
@@ -50,15 +52,36 @@ const Hero = () => {
 
 				<div className="figures">
 					<div>
-						<span>+ 140</span>
+						<span>
+							<NumberCounter
+								end={140}
+								start={0}
+								delay="4"
+								preFix="+"
+							></NumberCounter>
+						</span>
 						<span>Expert Coaches</span>
 					</div>
 					<div>
-						<span>+ 978</span>
+						<span>
+							<NumberCounter
+								end={978}
+								start={0}
+								delay="4"
+								preFix="+"
+							></NumberCounter>
+						</span>
 						<span>Members Joined</span>
 					</div>
 					<div>
-						<span>+ 50</span>
+						<span>
+							<NumberCounter
+								end={50}
+								start={0}
+								delay="4"
+								preFix="+"
+							></NumberCounter>
+						</span>
 						<span>Fitness programs</span>
 					</div>
 				</div>
